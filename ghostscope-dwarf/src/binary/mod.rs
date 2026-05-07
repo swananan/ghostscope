f547e342b99621454daf6319c97fd4fa5068e79b
swananan <jt26wzz@gmail.com>
2026-05-07 20:57:28 +0800

    fix: load compressed dwarf sections

--- ghostscope-dwarf/src/binary/mod.rs
+++ ghostscope-dwarf/src/binary/mod.rs
@@ -2,9 +2,9 @@ pub(crate) mod debuglink;
 pub(crate) mod mapped_file;
 
 pub(crate) use debuglink::try_load_debug_file;
-pub(crate) use mapped_file::{
-    dwarf_endian_from_object, empty_dwarf_reader_with_endian, DwarfData, DwarfEndian, DwarfReader,
-    MappedFile,
-};
 #[cfg(test)]
-pub(crate) use mapped_file::{dwarf_reader_from_arc, dwarf_reader_from_arc_with_endian};
+pub(crate) use mapped_file::dwarf_reader_from_arc;
+pub(crate) use mapped_file::{
+    dwarf_endian_from_object, dwarf_reader_from_arc_with_endian, empty_dwarf_reader_with_endian,
+    DwarfData, DwarfEndian, DwarfReader, MappedFile,
+};
